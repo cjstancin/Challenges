1bf8545fa6eda735cf0d18a0fc4f496e8521286f
cjstancin <cjstancin@outlook.com>
2020-10-30 11:10:24 -0600

    Recommiting code, broke last time

--- Color.java
+++ Color.java
@@ -2,23 +2,67 @@ import java.util.*;
 
 public class Color {
     Random colorGenerator = new Random();
-    int greenValue;
-    int redValue;
-    int blueValue;
+    int red;
+    int green;
+    int blue;
 
-    public Color(int green, int red, int blue) {
-        greenValue = green;
-        redValue = red;
-        blueValue = blue;
+    public Color(int red, int green, int blue) {
+        setRed((red));
+        setGreen((green));
+        setBlue((blue));
     }
 
     public Color() {
-        greenValue = colorGenerator.nextInt(256);
-        redValue = colorGenerator.nextInt(256);
-        blueValue = colorGenerator.nextInt(256);
+        setRed(colorGenerator.nextInt(256));
+        setGreen(colorGenerator.nextInt(256));
+        setBlue(colorGenerator.nextInt(256));
+        
     }
 
-    public void 
+    public void randomize() {
+        setRed(colorGenerator.nextInt(256));
+        setGreen(colorGenerator.nextInt(256));
+        setBlue(colorGenerator.nextInt(256));
+    }
+
+    private int colorCorrect(int input) {
+        if (input > 255) {
+            input = 255;
+        }
+        if (input < 0) {
+            input = 0;
+        }
+        return input;
+    }
+
+    public void setRed(int red) {this.red = colorCorrect(red);}
+    public void setGreen(int green) {this.green = colorCorrect(green);}
+    public void setBlue(int blue) {this.blue = colorCorrect(blue);}
+    public int getRed() {return red;}
+    public int getGreen() {return green;}
+    public int getBlue() {return blue;}
+
+    public void darkenRed(double percent) {
+        this.red = (int)(red * (1 + (-1 * percent)));
+    }
+    public void darkenGreen(double percent) {
+        this.green = (int)(green * (1 + (-1 * percent)));
+    }
+    public void darkenBlue(double percent) {
+        this.blue = (int)(blue * (1 + (-1 * percent)));
+    }
+    public void LightenRed(double percent) {
+        this.red = (int)(red * (1 + (percent)));
+    }
+    public void LightenGreen(double percent) {
+        this.green = (int)(green * (1 + (percent)));
+    }
+    public void LightenBlue(double percent) {
+        this.blue = (int)(blue * (1 + (percent)));
+    }
     
+    public String toString() {
+        return "[" + red +", " + green + ", " + blue + "]";
+    }
     
 }
--- ColorTester.java
+++ ColorTester.java
@@ -1,9 +1,14 @@
 class ColorTester {
     public static void main(String[] args) {
-        Color color1= new Color(123,123,123);
+        Color color1= new Color(-2,55,257);
         System.out.println(color1);
-        Color color2= new Color();
-        System.out.println(color2);
-        
+        color1.randomize();
+        System.out.println(color1);
+        color1.setGreen(122);
+        System.out.println(color1);
+        color1.darkenBlue(0.1);
+        System.out.println(color1);
+        color1.LightenBlue(0.1);
+        System.out.println(color1);
     }
 }
